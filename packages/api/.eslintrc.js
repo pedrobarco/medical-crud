9c5ef9fd90f09aff6e04e07f89d5e7b3d4bad09f
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2021-04-07 23:07:00 +0100

    fix code format on save

--- packages/api/.eslintrc.js
+++ packages/api/.eslintrc.js
@@ -3,17 +3,13 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['standard'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
   },
-  plugins: [
-    '@typescript-eslint'
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
   }
 }
